48a06a97d7e18db1addce4076eb3c3be9ec52040
Gursagar Singh <gss.anandi2002@gmail.com>
2023-01-22 13:45:49 +0530

    Initial Commit
    A program in C that inputs and stores student details of a school and retrieves the information when required
    
    This program uses a struct to store the details of each student, including the name, age, and roll number. The input() function prompts the user to enter the number of students and then collects the details for each student. The display() function takes a roll number as an argument and searches for the student with that roll number in the struct, printing their details if found.

---
+++ Students_Data.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student {
+    char name[50];
+    int age;
+    int roll_no;
+};
+
+struct student s[50];
+int n;
+
+void input() {
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter the details of student %d\n", i + 1);
+        printf("Enter name: ");
+        scanf("%s", s[i].name);
+        printf("Enter age: ");
+        scanf("%d", &s[i].age);
+        printf("Enter roll number: ");
+        scanf("%d", &s[i].roll_no);
+    }
+}
+
+void display(int rn) {
+    int flag = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i].roll_no == rn) {
+            printf("Name: %s\n", s[i].name);
+            printf("Age: %d\n", s[i].age);
+            printf("Roll Number: %d\n", s[i].roll_no);
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        printf("Roll number not found!\n");
+}
+
+int main() {
+    input();
+    int rn;
+    printf("Enter the roll number of the student whose details you want to see: ");
+    scanf("%d", &rn);
+    display(rn);
+    return 0;
+}
